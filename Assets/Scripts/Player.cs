0823472956cc9bce72e54454b456f7ab927a337a
Ioan Oltean <ionut.oltean@quadom.com>
2023-05-03 11:17:03 +0300

    Player Basic Movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,6 +13,26 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 inputVector = new Vector2(0, 0);
+        if (Input.GetKey((KeyCode.W)))
+        {
+            inputVector.y += 1*Time.deltaTime;
+        }
+        if (Input.GetKey((KeyCode.S)))
+        {
+            inputVector.y -= 1*Time.deltaTime;
+        }
+        if (Input.GetKey((KeyCode.D)))
+        {
+            inputVector.x +=1*Time.deltaTime;
+        }
+        if (Input.GetKey((KeyCode.A)))
+        {
+            inputVector.x -= 1*Time.deltaTime;
+        }
+
+        inputVector = inputVector.normalized;
+        Vector3 moveDir =  new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDir;
     }
 }
