7898a2ebdf36510320674a06a9c48b5d635f326b
Ioan Oltean <ionut.oltean@quadom.com>
2023-05-03 12:00:35 +0300

    player rotate

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField]
-    private float _moveSpeed = 5;
+    [SerializeField] private float _moveSpeed = 5;
+    [SerializeField] private float _rotateSpeed = 10;
 
     // Update is called once per frame
     void Update()
@@ -13,23 +13,27 @@ public class Player : MonoBehaviour
         Vector2 inputVector = new Vector2(0, 0);
         if (Input.GetKey((KeyCode.W)))
         {
-            inputVector.y += 1*Time.deltaTime;
+            inputVector.y += 1 * Time.deltaTime;
         }
+
         if (Input.GetKey((KeyCode.S)))
         {
-            inputVector.y -= 1*Time.deltaTime;
+            inputVector.y -= 1 * Time.deltaTime;
         }
+
         if (Input.GetKey((KeyCode.D)))
         {
-            inputVector.x +=1*Time.deltaTime;
+            inputVector.x += 1 * Time.deltaTime;
         }
+
         if (Input.GetKey((KeyCode.A)))
         {
-            inputVector.x -= 1*Time.deltaTime;
+            inputVector.x -= 1 * Time.deltaTime;
         }
 
         inputVector = inputVector.normalized;
-        Vector3 moveDir =  new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += _moveSpeed*moveDir;
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime*_rotateSpeed);
+        transform.position += _moveSpeed * moveDir;
     }
 }
